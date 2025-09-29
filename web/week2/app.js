e8d8cf37f284a0032038cdcb6a8c1a4b7857d0a6
Annie Vu <hanhannievu@gmail.com>
2025-09-29 17:20:34 -0400

    week 2 activities

---
+++ web/week2/app.js
@@ -0,0 +1,11 @@
+console.log('Hello World');
+alert('Hello again world!');
+
+function fahrenheitToCelsius(fahrenheit) {
+    let celsius = (fahrenheit - 32) * 5 / 9;
+    return celsius;
+}
+
+if (temp > 90) {
+    console.log('It is hot out!');
+}
