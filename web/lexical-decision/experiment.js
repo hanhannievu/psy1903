4f94493a9803e1a2f2c11b8400a7ea3927f87126
Annie Vu <hanhannievu@gmail.com>
2025-10-13 14:40:19 -0400

    Enhance survey experiences

--- web/lexical-decision/experiment.js
+++ web/lexical-decision/experiment.js
@@ -1,6 +1,30 @@
-let jsPsych = initJsPsych();
+let jsPsych = initJsPsych({
+    show_progress_bar: true
+});
 
 let timeline = [];
+
+let ageCheckTrial = {
+    type: jsPsychSurveyHtmlForm,
+    html: `
+    <h1>Welcome!</h1> 
+    Please enter your age to continue: <input type='text' name='age' id='age'>
+    `,
+    autofocus: 'age',
+    on_finish: function (data) {
+        if (data.response.age < 18) {
+            jsPsych.abortExperiment('You must be 18 years or older to complete this experiment.');
+        }
+    }
+}
+timeline.push(ageCheckTrial);
+
+let enterFullScreenTrial = {
+    type: jsPsychFullscreen,
+    fullscreen_mode: true
+};
+
+timeline.push(enterFullScreenTrial);
 
 //Welcome 
 let welcomeTrial = {
@@ -8,15 +32,37 @@ let welcomeTrial = {
     stimulus: `
     <h1>Welcome to the Lexical Decision Task</h1>
     <p>You are about to see a series of characters.</p> 
-    <p>Press F if the characters make up a word.</p> 
-    <p>Press J if the characters do not make up a word.</p> 
-    <p class = 'instructions'>Press SPACE to begin</p>
+    <p>Press <span class = 'key'>F</span> if the characters make up a word.</p> 
+    <p>Press <span class = 'key'>J</span> if the characters do not make up a word.</p> 
+    <p class = 'instructions'>Press <span class = 'key'>SPACE</span> to begin</p>
     `,
     choices: [' '],
 };
 timeline.push(welcomeTrial);
 
+let primeTrial = {
+    type: jsPsychHtmlKeyboardResponse,
+    stimulus: `
+        <p>You were randomly chosen to see this trial.</p> 
+        <p>Press the <span class = 'key'>SPACE</span> key to continue.</p>
+        `,
+    choices: [' '],
+    data: {
+        collect: true,
+        trialType: 'prime',
+    },
+    on_load: function () {
+        if (getRandomNumber(0, 1) == 0) {
+            jsPsych.data.addProperties({ sawPrime: false });
+            jsPsych.finishTrial();
+        } else {
+            jsPsych.data.addProperties({ sawPrime: true });
+        }
+    }
+}
+timeline.push(primeTrial);
 
+//Conditions
 for (let block of conditions) {
 
     let blockConditions = jsPsych.randomization.repeat(block.conditions, 1);
@@ -28,7 +74,7 @@ for (let block of conditions) {
             <p>You are about to see a series of ${block.count} characters.</p>
             <p>If the characters make up a word, press the F key.</p>
             <p>If the characters do not make up a word, press the J key.</p>
-            <p>Press SPACE to begin.</p>
+            <p class = 'instructions'>Press <span class = 'key'>SPACE</span> to begin.</p>
             `,
         choices: [' '],
     };
@@ -42,6 +88,7 @@ for (let block of conditions) {
             stimulus: `<h1>${condition.characters}</h1>`,
             choices: ['f', 'j'],
             data: {
+                trialType: 'mainTrials',
                 collect: true,
                 characters: condition.characters,
                 blockId: block.title,
@@ -49,7 +96,7 @@ for (let block of conditions) {
             on_finish: function (data) {
                 if (data.response == 'f' && condition.isWord == true) {
                     data.correct = true;
-                } else if (data.correct == 'j' && condition.isWord == false) {
+                } else if (data.response == 'j' && condition.isWord == false) {
                     data.correct = true;
                 } else {
                     data.correct = false;
@@ -57,8 +104,25 @@ for (let block of conditions) {
             }
         }
         timeline.push(conditionTrial);
+
+        //Give feedback
+        let feedbackTrial = {
+            type: jsPsychHtmlKeyboardResponse,
+            stimulus: `<h1 class = 'incorrect'>X</h1>`,
+            trial_duration: 1000,
+            choices: ['NO KEY'],
+            on_load: function () {
+                let lastTrialData = jsPsych.data.getLastTrialData().values()[0];
+                if (lastTrialData.correct) {
+                    // Force skip this feedback trial if they got the previous trial correct
+                    jsPsych.finishTrial();
+                }
+            },
+        }
+        timeline.push(feedbackTrial);
     };
 };
+
 
 let resultsTrial = {
     type: jsPsychHtmlKeyboardResponse,
@@ -114,6 +178,13 @@ let resultsTrial = {
 }
 timeline.push(resultsTrial);
 
+let exitFullScreenTrial = {
+    type: jsPsychFullscreen,
+    fullscreen_mode: false
+};
+timeline.push(exitFullScreenTrial);
+
+
 let debriefTrial = {
     type: jsPsychHtmlKeyboardResponse,
     stimulus: `
@@ -121,6 +192,9 @@ let debriefTrial = {
     <p>You can close this tab.</p>
     `,
     choices: ['NO KEYS'],
+    on_start: function () {
+        jsPsych.progressBar.progress = 1;
+    }
 };
 
 timeline.push(debriefTrial);
@@ -128,3 +202,7 @@ timeline.push(debriefTrial);
 //Debrief
 
 jsPsych.run(timeline)
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
